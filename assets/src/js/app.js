dc09c653a52e680d61f3d187e8a17d71a01dc286
itsumrat <itsumrat@gmail.com>
2017-02-12 00:30:02 +0600

    home page

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -1,11 +1,3 @@
-jQuery(document).ready(function($){
-
-	/*Hide menu by default*/
-	$("#menu").hide();
-
-	/*When menu button is clicked, toggle the menu*/
-	$("#menu-btn").click(function(){
-	  $("#menu").slideToggle();
-	});
-
+$(document).ready(function(){
+	
 });
